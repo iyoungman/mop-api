6111db0ce414ba0facf2447317e7e34e322ae471
iyoungman <hsl9546@gmail.com>
2019-05-12 22:28:17 +0900

    git init

---
+++ src/main/java/com/youngman/mop/MopApplication.java
@@ -0,0 +1,13 @@
+package com.youngman.mop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MopApplication.class, args);
+	}
+
+}
