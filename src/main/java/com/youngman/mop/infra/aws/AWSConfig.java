047e967e955804049e01c30cc90fa0e755233662
iyoungman <hsl9546@gmail.com>
2019-07-18 14:12:40 +0900

    S3 EC2 설정 변경

--- src/main/java/com/youngman/mop/infra/aws/AWSConfig.java
+++ src/main/java/com/youngman/mop/infra/aws/AWSConfig.java
@@ -16,24 +16,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AWSConfig {
 
-	@Value("${cloud.aws.credentials.accessKey}")
-	private String accessKey;
-
-	@Value("${cloud.aws.credentials.secretKey}")
-	private String secretKey;
+//	@Value("${cloud.aws.credentials.accessKey}")
+//	private String accessKey;
+//
+//	@Value("${cloud.aws.credentials.secretKey}")
+//	private String secretKey;
 
 	@Value("${cloud.aws.region.static}")
 	private String region;
 
 
-	@Bean
-	public BasicAWSCredentials basicAWSCredentials() {
-		return new BasicAWSCredentials(accessKey, secretKey);
-	}
+//	@Bean
+//	public BasicAWSCredentials basicAWSCredentials() {
+//		return new BasicAWSCredentials(accessKey, secretKey);
+//	}
 
 	@Bean
-	public AmazonS3Client amazonS3Client(AWSCredentials awsCredentials) {
-		AmazonS3Client amazonS3Client = new AmazonS3Client(awsCredentials);
+	public AmazonS3Client amazonS3Client() {
+//		AmazonS3Client amazonS3Client = new AmazonS3Client(awsCredentials);
+		AmazonS3Client amazonS3Client = new AmazonS3Client();
 		amazonS3Client.setRegion(Region.getRegion(Regions.fromName(region)));
 		return amazonS3Client;
 	}
