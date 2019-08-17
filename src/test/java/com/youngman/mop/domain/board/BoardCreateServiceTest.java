b8a92b59d5b03a288c2eda3493582afdebd445d6
iyoungman <hsl9546@gmail.com>
2019-08-18 03:02:39 +0900

    게시글 저장 Test

---
+++ src/test/java/com/youngman/mop/domain/board/BoardCreateServiceTest.java
@@ -0,0 +1,78 @@
+package com.youngman.mop.domain.board;
+
+import com.youngman.mop.domain.board.dao.BoardRepository;
+import com.youngman.mop.domain.board.domain.Board;
+import com.youngman.mop.domain.board.domain.BoardType;
+import com.youngman.mop.domain.board.dto.BoardCreateRequest;
+import com.youngman.mop.domain.board.service.BoardCreateService;
+import com.youngman.mop.domain.club.dao.ClubFindDao;
+import com.youngman.mop.domain.club.domain.Club;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.ArgumentMatchers.refEq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by YoungMan on 2019-08-09.
+ */
+
+@NoArgsConstructor
+@ExtendWith(SpringExtension.class)
+class BoardCreateServiceTest {
+
+	@InjectMocks
+	private BoardCreateService boardCreateService;
+
+	@Mock
+	private BoardRepository boardRepository;
+
+	@Mock
+	private ClubFindDao clubFindDao;
+
+	private Long CLUB_ID = 1L;
+	private Club club;
+	private BoardCreateRequest boardCreateRequest;
+	private Board board;
+
+
+	@BeforeEach
+	void setUp() {
+		club = Club.builder()
+				.name("name")
+				.region("region")
+				.chairEmail("chairEmail")
+				.hobby("hobby")
+				.introduce("introduce")
+				.build();
+
+		boardCreateRequest = BoardCreateRequest.builder()
+				.clubId(CLUB_ID)
+				.title("title")
+				.writer("writer")
+				.content("content")
+				.boardType(BoardType.POST)
+				.build();
+
+		board = boardCreateRequest.toEntity(club);
+	}
+
+	@Test
+	void 게시글_저장() {
+		//given
+		when(clubFindDao.findById(CLUB_ID)).thenReturn(club);
+		when(boardRepository.save(board)).thenReturn(board);
+
+		//when
+		boardCreateService.createBoard(boardCreateRequest);
+
+		//then
+		verify(boardRepository).save(refEq(board));
+	}
+}
