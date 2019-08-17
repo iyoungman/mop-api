aabcb8931c8e04b66a64a083090ef398cbbe6e82
iyoungman <hsl9546@gmail.com>
2019-08-18 02:48:39 +0900

    Git 코드 정렬 Test

--- src/main/java/com/youngman/mop/domain/board/dto/BoardPagingResponse.java
+++ src/main/java/com/youngman/mop/domain/board/dto/BoardPagingResponse.java
@@ -19,20 +19,20 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BoardPagingResponse {
 
-	private boolean isLast;
-	private boolean isEmpty;
-	private List<Board> notices = new ArrayList<>();
-	private List<Board> posts = new ArrayList<>();
+    private boolean isLast;
+    private boolean isEmpty;
+    private List<Board> notices = new ArrayList<>();
+    private List<Board> posts = new ArrayList<>();
 
 
-	@Builder
-	public BoardPagingResponse(boolean isLast, boolean isEmpty,
-							   List<Board> notices, List<Board> posts) {
-		this.isLast = isLast;
-		this.isEmpty = isEmpty;
-		this.notices = notices;
-		this.posts = posts;
-	}
+    @Builder
+    public BoardPagingResponse(boolean isLast, boolean isEmpty,
+                               List<Board> notices, List<Board> posts) {
+        this.isLast = isLast;
+        this.isEmpty = isEmpty;
+        this.notices = notices;
+        this.posts = posts;
+    }
 
 
 }
