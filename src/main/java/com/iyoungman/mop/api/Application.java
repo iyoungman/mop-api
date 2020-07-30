d8daa4a66fb96f3e04d5004490c187a0acacbcc5
iyoungman <imiyoungman@daum.net>
2020-07-30 15:54:33 +0900

    Update Application

--- src/main/java/com/iyoungman/mop/api/Application.java
+++ src/main/java/com/iyoungman/mop/api/Application.java
@@ -1,13 +1,28 @@
 package com.iyoungman.mop.api;
 
-import org.springframework.boot.SpringApplication;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
+    private static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml";
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .bannerMode(Banner.Mode.OFF)
+                .properties(APPLICATION_LOCATIONS)
+                .build()
+                .run(args);
     }
 
 }
